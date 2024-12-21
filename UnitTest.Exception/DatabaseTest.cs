0971059bc275119213f679538fdd708ba857f7e7
viola.viventi <viola.viventi@gmail.com>
2024-12-21 15:40:18 +0100

    c

---
+++ UnitTest.Exception/DatabaseTest.cs
@@ -0,0 +1,117 @@
+using System.Data.Common;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Moq;
+
+using BusinessLogic.Options;
+using BusinessLogic.Services;
+using Data.Data.EF.Context;
+using Data.Data.EF.Output;
+
+namespace BusinessLogicTest
+{
+    public class DatabaseTest
+    {
+        private NorthwindContext CreateDbContext()
+        {
+            var options = new DbContextOptionsBuilder<NorthwindContext>()
+                .UseSqlServer("Server=localhost\\SQLEXPRESS;Database=Northwind;Trusted_Connection=True;Integrated Security=SSPI;TrustServerCertificate=True;").Options;
+            var dbContext = new NorthwindContext(options);
+            return dbContext;
+        }
+
+        [Fact]
+        public void It_should_add_a_product_successfully_into_data_store()
+        {
+            //Arrange
+            var dbContext = CreateDbContext();            
+            var dati = dbContext
+                   .Categories
+                   .Take(5)
+                   .AsEnumerable();
+            //Assert
+            Assert.True(dati.Count() == 5);
+            //Clean up
+            dbContext.Dispose();
+        }
+
+        [Fact]
+        public async Task It_should_add_a_product_successfully_into_data_store_2()
+        {
+            var mockSet = new Mock<Microsoft.EntityFrameworkCore.DbSet<Category>>();
+
+            var mockContext = new Mock<NorthwindContext>();
+            var options = new Mock<IOptions<BusinessOptions>>();
+            mockContext.Setup(m => m.Categories).Returns(mockSet.Object);
+
+            var service = new BusinessServices(mockContext.Object, options.Object);
+            var data = await service.GetData();
+
+            Assert.True(data.Count() == 1);
+        }
+        [Fact]
+        public async Task It_should_add_a_product_successfully_into_data_store_3()
+        {
+            var mockSet = new Mock<Microsoft.EntityFrameworkCore.DbSet<Category>>();
+
+            var mockContext = new Mock<NorthwindContext>();
+            var options = new Mock<IOptions<BusinessOptions>>();
+
+            var data = new List<Category>
+            {
+                (new Mock<Category>()).Object,
+                (new Mock<Category>()).Object,
+                (new Mock<Category>()).Object,
+                (new Mock<Category>()).Object,
+                (new Mock<Category>()).Object,
+                (new Mock<Category>()).Object
+            }.AsQueryable();
+
+            mockSet.As<IEnumerable<Category>>()
+                .Setup(m => m.GetEnumerator())
+                .Returns(data.GetEnumerator());
+
+            mockContext.Setup(m => m.Categories).Returns(mockSet.Object);
+
+            var service = new BusinessServices(mockContext.Object, options.Object);
+            var result = await service.GetData();
+
+            Assert.True(result.Count() == 6);
+        }
+
+
+
+        /**************************+ DAPPER ********************************/
+        [Fact]
+        public async Task DapperTest()
+        {
+            var mockSet = new Mock<Microsoft.EntityFrameworkCore.DbSet<Category>>();
+
+            var mockContext = new Mock<NorthwindContext>();
+            var options = new Mock<IOptions<BusinessOptions>>();
+            mockContext.Setup(m => m.Database.GetDbConnection()).Returns((new Mock<DbConnection>()).Object);
+
+            var data = new List<Category>
+            {
+                (new Mock<Category>()).Object,
+                (new Mock<Category>()).Object,
+                (new Mock<Category>()).Object,
+                (new Mock<Category>()).Object,
+                (new Mock<Category>()).Object,
+                (new Mock<Category>()).Object
+            }.AsQueryable();
+
+            mockSet.As<IEnumerable<Category>>()
+                .Setup(m => m.GetEnumerator())
+                .Returns(data.GetEnumerator());
+
+            var service = new BusinessServices(mockContext.Object, options.Object);
+            var result = await service.GetCategories();
+
+            Assert.True(result.Count() == 6);
+        }
+        /*********************************** FINE DAPPER ********************************/
+
+    }
+}
---
+++ UnitTest.Exception/ExceptionTest.cs
@@ -0,0 +1,90 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Options;
+using Moq;
+using Newtonsoft.Json;
+
+using BusinessLogic.Options;
+using BusinessLogic.Services;
+using Data.Data.EF.Context;
+using Vi.Service.Exception.Models;
+
+namespace BusinessLogicTest;
+//from https://learn.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.testhost.testserver?view=aspnetcore-9.0
+
+// <snippet1>
+public class BasicTests
+    : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public BasicTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+    
+    [Theory]
+    [InlineData(5)]
+    public async Task TestThrowErrors(int data)
+    {
+        var mockMessageHandler = new Mock<HttpMessageHandler>();
+        var mockContext = new Mock<NorthwindContext>();
+        var options = new Mock<IOptions<BusinessOptions>>();
+        var service = new BusinessServices(mockContext.Object, options.Object);
+        var apiRestCall = await service.ThrowErrors(data);
+        Assert.Throws<System.Exception>(() => { });
+    }
+
+    [Theory]
+    [InlineData("Api/v1/GetData")]
+    public async Task GetDataHttpResponseOK(string url)
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync(url);
+        var content = await response.Content.ReadAsStringAsync();
+
+        // Assert
+        response.EnsureSuccessStatusCode(); // Status Code 200-299
+        Assert.Equal("application/json; charset=utf-8",
+            response.Content.Headers.ContentType.ToString());
+    }
+
+    [Theory]
+    [InlineData("Api/v1/ThrowErrors/5")]
+    public async Task ThrowErrorsResponseKO(string url)
+    {
+        var client = _factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync(url);
+        var content = await response.Content.ReadAsStringAsync();
+        var typedContent = JsonConvert.DeserializeObject<DomainDataError>(content);
+
+        Assert.True(response.StatusCode == HttpStatusCode.InternalServerError);
+    }
+
+    [Theory]
+    [InlineData("Api/v1/ThrowErrors/5")]
+    public async Task ThrowsErrorsData(string url)
+    {
+        var client = _factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync(url);
+        var content = await response.Content.ReadAsStringAsync();
+        var typedContent = JsonConvert.DeserializeObject<DomainDataError>(content);
+
+        var kv1 = new KeyValuePair<string, object?>(
+            BusinessLogic.DomainDataError.BusinessError.BusinessMatErrors.CategoryAlreadyExists.Item1.ToString(),
+            BusinessLogic.DomainDataError.BusinessError.BusinessMatErrors.CategoryAlreadyExists.Item2);
+        Assert.True(typedContent.Details.Contains(kv1));
+        Assert.True(typedContent.Details.Contains(new KeyValuePair<string, object?>(
+            BusinessLogic.DomainDataError.BusinessError.BusinessMatErrors.CategoryAlreadyExists2.Item1.ToString(),
+            BusinessLogic.DomainDataError.BusinessError.BusinessMatErrors.CategoryAlreadyExists2.Item2)));
+    }
+
+
+}  

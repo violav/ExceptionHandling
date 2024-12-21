f4fa434b2e4f06353801a7b655362093bfefaa56
viola.viventi <viola.viventi@gmail.com>
2024-12-21 17:06:11 +0100

    refactoring

--- UnitTest.Exception/ExceptionTest.cs
+++ UnitTest.Exception/ExceptionTest.cs
@@ -7,7 +7,17 @@ using Newtonsoft.Json;
 using BusinessLogic.Options;
 using BusinessLogic.Services;
 using Data.Data.EF.Context;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
 using Vi.Service.Exception.Models;
+using Microsoft.AspNetCore.Builder;
+using Vi.Service.Exception;
+using Vi.Service.Exception.Extensions;
+using BusinessLogic.Extensions;
+using Data.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BusinessLogicTest;
 //from https://learn.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.testhost.testserver?view=aspnetcore-9.0
@@ -87,4 +97,59 @@ public class BasicTests
     }
 
 
+    
+    //[Fact]
+    //public async Task MiddlewareTest_ReturnsNotFoundForRequest()
+    //{
+    //    Mock<IConfigurationSection> mockSection = new Mock<IConfigurationSection>();
+    //    mockSection.Setup(x => x.Value).Returns("BusinessOptions");
+
+    //    Mock<IConfiguration> mockConfig = new Mock<IConfiguration>();
+    //    mockConfig.Setup(x => x.GetSection("BusinessOptions")).Returns(mockSection.Object);
+    //    mockSection.Setup(x => x.Value).Returns("DataOptions");
+    //    mockConfig.Setup(x => x.GetSection("DataOptions")).Returns(mockSection.Object);
+
+    //    using var host = await new HostBuilder()
+    //        .ConfigureWebHost(webBuilder =>
+    //        {
+    //            webBuilder
+    //                .UseTestServer()
+    //                .ConfigureServices(services =>
+    //                {
+    //                    services.AddExceptions()
+    //                        .AddBusiness(mockConfig.Object)
+    //                        ;
+    //                    services
+    //                        .AddOptions();
+
+    //                    services
+    //                        .AddCors()
+    //                        .AddControllers();
+
+    //                    services
+    //                        .AddEndpointsApiExplorer()
+    //                        .AddSwaggerGen();
+    //                    services.AddDataContext2(mockConfig.Object);
+
+    //                })
+    //                .Configure(app =>
+    //                {
+    //                    app.UseMiddleware<ExceptionMiddleware>();
+    //                });
+    //        })
+    //        .StartAsync();
+
+    //    var response = await host.GetTestClient().GetAsync("/Api/v1/ThrowErrors/5");
+    //    var content = await response.Content.ReadAsStringAsync();
+    //    var typedContent = JsonConvert.DeserializeObject<DomainDataError>(content);
+
+    //    var kv1 = new KeyValuePair<string, object?>(
+    //        BusinessLogic.DomainDataError.BusinessError.BusinessMatErrors.CategoryAlreadyExists.Item1.ToString(),
+    //        BusinessLogic.DomainDataError.BusinessError.BusinessMatErrors.CategoryAlreadyExists.Item2);
+    //    Assert.True(typedContent.Details.Contains(kv1));
+    //    Assert.True(typedContent.Details.Contains(new KeyValuePair<string, object?>(
+    //        BusinessLogic.DomainDataError.BusinessError.BusinessMatErrors.CategoryAlreadyExists2.Item1.ToString(),
+    //        BusinessLogic.DomainDataError.BusinessError.BusinessMatErrors.CategoryAlreadyExists2.Item2)));
+
+    //}
 }  
